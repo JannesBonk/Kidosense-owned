beaa2030bb98a1066a671f51676e5a3b81f660ed
JannesBonk <86556266+JannesBonk@users.noreply.github.com>
2023-05-14 09:11:40 +0300

    Create UNICODE_STRING.cs

---
+++ Loader source/loader/ManualMapInjection/Injection/Win32/UNICODE_STRING.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ManualMapInjection.Injection.Win32
+{
+	// Token: 0x0200001A RID: 26
+	public struct UNICODE_STRING
+	{
+		// Token: 0x04000108 RID: 264
+		public ushort Length;
+
+		// Token: 0x04000109 RID: 265
+		public ushort MaximumLength;
+
+		// Token: 0x0400010A RID: 266
+		public IntPtr Buffer;
+	}
+}
